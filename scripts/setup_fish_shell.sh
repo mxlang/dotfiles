d6cf42409c8c612d79c360ffba61478ea241025b
Maximilian Lang <lang.maximilian1997@gmail.com>
2026-01-09 17:53:56 +0100

    add fish shell

--- scripts/setup_fish_shell.sh
+++ scripts/setup_fish_shell.sh
@@ -2,12 +2,13 @@
 
 set -e
 
-brew install fish
+if ! command -v brew >/dev/null 2>&1; then
+    brew install fish
+fi
 
 FISH_PATH="$(command -v fish)"
 
 if [[ -n "$FISH_PATH" ]] && ! grep -qx "$FISH_PATH" /etc/shells; then
     echo "$FISH_PATH" | sudo tee -a /etc/shells > /dev/null
+    chsh -s "$(command -v fish)"
 fi
-
-chsh -s "$(command -v fish)"
